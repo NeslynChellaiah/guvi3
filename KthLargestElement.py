9150b64f18270fa0474f1eb4784d7c255cfc8d42
Neslyn Chellaiah <neslynchellaiah@gmail.com>
2019-03-30 18:24:41 +0530

    Rename kthLargestElement.py to KthLargestElement.py

---
+++ KthLargestElement.py
@@ -0,0 +1,12 @@
+class main():
+    def func(self):
+        N,K = input().split()
+        N = int(N)
+        K = int(K)
+        a = []
+        a = input().split()
+        a.sort()
+        x = N-K
+        print(a[x])
+ob = main()
+ob.func()

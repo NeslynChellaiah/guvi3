6e03e29923cfb557ade1321dc187fb575af4641b
Neslyn Chellaiah <neslynchellaiah@gmail.com>
2019-04-12 20:24:58 +0530

    Update indexAndValue.py

--- indexAndValue.py
+++ indexAndValue.py
@@ -2,16 +2,22 @@ class main():
     def func(self):
         a = int(input())
         l = []
-        l = input().split()
+        z = []
+        l = [int(x) for x in input().split()]
         count = 0
         for i in range (0,a):
             b = int(l[i])
-            if (i==b) and (i<a-1):
-                print (b,end=' ')
-            elif (i==b) and (i==a-1):
-                print (b,end='')
+            if (i==b) and (i<a):
+                z.append(b)
             else:
                 count+=1
+
+        lz = len(z)
+        for i in range (0,lz):
+            if (i<lz-1):
+                print (z[i],end=' ')
+            elif (i==lz-1):
+                print (z[i],end='')
         if (count == a):
             print (-1)
 
